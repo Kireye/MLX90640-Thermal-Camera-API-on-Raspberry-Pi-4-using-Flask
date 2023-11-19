631dd4311d7cd4bddc074c97a7827ffc8da8ba3f
Henrik Werkström <henrik@werkstrom.com>
2023-11-19 11:35:48 +0100

    Create app.py
    Added functionality for capturing images from Pi camera at /capture
    Moved thermal service to /thermal
    Added info at / (root)

---
+++ app.py
@@ -0,0 +1,46 @@
+import board,busio
+import adafruit_mlx90640
+import numpy as np
+from flask import Flask, jsonify
+from picamera2 import Picamera2
+import base64
+
+app = Flask(__name__)
+
+#Sets up the thermal camera
+i2c = busio.I2C(board.SCL, board.SDA, frequency=400000) #Sets up i2c to work
+mlx = adafruit_mlx90640.MLX90640(i2c) #Defines mlx
+mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_1_HZ #Sets the refresh rate of the camera.
+
+@app.route("/")
+def index():
+    return "Returns a JSON resonse<br>Use /thermal for an array of temperatures <br>Use /capture for a base64 encoded image"
+
+@app.route('/thermal')
+
+def thermal():
+        frame = np.zeros(768) #Creates frame array. Change 768 to the number of pixels that your camera has in total if you are not using an MLX90640. My camera has a resolution of 32x24 which makes the total pixels 768.
+        mlx.getFrame(frame) #Gets the temperature information from the camera and enters it into the frame array
+        frameString = ','.join(str(x) for x in frame) #Converts frame into a , seperated string
+        ##return frameString #Returns the captured temperature of the camera's pixels as a , seperated string
+        return jsonify({"temperatures": frame.tolist()})
+
+@app.route('/capture')
+def capture():
+    picam2 = Picamera2()
+    config = picam2.create_still_configuration()
+    picam2.configure(config)
+
+    picam2.start()
+
+    theImage = picam2.capture_image()
+    theImageBytes = theImage.tobytes()
+    theImageBase64 = base64.b64encode(theImageBytes).decode('utf-8')
+    
+    picam2.stop()
+    picam2.close()
+
+    return jsonify({"image": theImageBase64})
+
+if __name__ == '__main__':
+        app.run(debug=True, host='0.0.0.0') #Enables debug and sets host
